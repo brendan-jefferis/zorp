c88e64d1cdd3cd226d09c753397b482895c83845
Brendan Jefferis <brendan.jefferis@gmail.com>
2017-11-05 18:34:10 +1100

    Improvements to input & output style

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ function render() {
   const output = document.getElementById('output')
   output.innerHTML = ''
   history.forEach(function (text) {
-  	const line = document.createElement('div')
-  	line.innerText = text
-  	output.appendChild(line)
+    const line = document.createElement('div')
+    line.innerText = text
+    output.appendChild(line)
+    output.scrollTop = output.scrollHeight
   })
 }
 
@@ -16,21 +17,22 @@ function focus (selector) {
   if (el) el.focus()
 }
 
-history.push('Hello there.')
-render()
 focus('input')
-
 document.addEventListener('click', function() { focus('input') })
 
 function handleKeyUp(e) {
-	if (e.keyCode !== 13) return
+  if (e.keyCode !== 13) {
+    return
+  }
 
-	history.push(e.currentTarget.value)
-	e.currentTarget.value = ''
-	render()
-	window.scrollTo(0,document.body.scrollHeight)
+  history.push(e.currentTarget.value)
+  e.currentTarget.value = ''
+  render()
 }
 
 const input = document.querySelector('input')
 input.value = ''
 input.addEventListener('keyup', handleKeyUp)
+
+history.push('Hello there.')
+render()
